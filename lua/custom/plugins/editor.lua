91d3fa6e4ee23e01f234cd19c15ae88ebf5f3431
Alexander Hutton <alexander@nosoft.biz>
2026-07-01 08:03:48 +0200

    Added obsession

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -1,4 +1,35 @@
 return {
+  {
+    'tpope/vim-obsession',
+    lazy = false,
+    config = function()
+      vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos'
+
+      local obsession = vim.api.nvim_create_augroup('obsession_autosave', { clear = true })
+
+      local function start_obsession()
+        if vim.fn.exists('g:SessionLoad') == 1 or vim.fn.exists('g:this_obsession') == 1 then
+          return
+        end
+
+        vim.cmd 'silent! Obsession'
+      end
+
+      vim.api.nvim_create_autocmd('VimEnter', {
+        group = obsession,
+        desc = 'Start obsession session tracking',
+        callback = function()
+          vim.schedule(start_obsession)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('DirChanged', {
+        group = obsession,
+        desc = 'Start obsession after changing directory',
+        callback = start_obsession,
+      })
+    end,
+  },
   'NMAC427/guess-indent.nvim',
   'tpope/vim-endwise',
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
